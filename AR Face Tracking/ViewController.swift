8713416b5510633a4c8337a6de31bfe6e867c93e
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-09-10 02:29:57 -0500

    create nodes with Face Geometry

--- AR Face Tracking/ViewController.swift
+++ AR Face Tracking/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Face Tracking not supported")
+        }
+        sceneView.delegate = self
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let configuration = ARFaceTrackingConfiguration()
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        sceneView.session.pause()
+    }
+}
 
 
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = sceneView.device else { return nil }
+        
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+        node.geometry?.firstMaterial?.fillMode = .lines
+        print(node)
+        return node
+    }
+    
 }
 
